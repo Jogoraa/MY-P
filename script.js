b08ca9fb7a5a27a487e7d92af5a8398a038f528d
jogoraa <davidtjogora@gmail.com>
2025-01-04 19:03:11 +0300

    half responsive

--- script.js
+++ script.js
@@ -83,10 +83,10 @@ mixitup(containerEl);
 const menuEl = document.querySelectorAll('header ul li a');
 let section = document.querySelectorAll('section')
 function activeMenu() {
-  let len=section.length
+  let len = section.length
 
-  while(--len &&  window.scrollY +97<section[len].offsetTop){} 
-  menuEl.forEach(sec=>sec.classList.remove('active'));
+  while (--len && window.scrollY + 97 < section[len].offsetTop) { }
+  menuEl.forEach(sec => sec.classList.remove('active'));
   menuEl[len].classList.add('active');
 }
 
@@ -94,6 +94,22 @@ function activeMenu() {
 // Sticky Nav bar------------------------
 const header = document.querySelector('header');
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   header.classList.toggle('sticky', window.scrollY > 50);
 })
+
+// toggle Icon  Nav bar------------------------
+
+let menuIcon = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
+};
+
+
+window.onscroll = () => {
+  menuIcon.classList.remove('bx-x');
+  navlist.classList.remove('open');
+};
